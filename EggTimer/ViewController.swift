8013e8775531f2100d609446db758c230db0ff15
Raphael Martins <64360469+raphaelalmeidamartins@users.noreply.github.com>
2023-05-27 16:04:18 -0300

    :sparkles: feat: implement timer logic

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,20 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let softTime = 5
-    let mediumTime = 7
-    let hardTime = 12
+    let cookingTimes = [
+        "Soft": 5,
+        "Medium": 7,
+        "Hard": 12
+    ]
+    
+    var elapsedTime: Double = 0.0
 
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        switch sender.currentTitle {
-            case "Soft":
-                print(softTime);
-            case "Medium":
-                print(mediumTime);
-            case "Hard":
-                print(hardTime);
-            default:
-                print("Invalid")
+        let time = cookingTimes[sender.currentTitle!]
+        
+        if time != nil { self.startTimer(time: time!) }
+        else { print("Error") }
+    }
+    
+    func startTimer(time: Int) {
+        let timeInSeconds: Double = Double(time) * 60.0
+    
+        let timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+            self.elapsedTime += timer.timeInterval
+            
+            if self.elapsedTime <= timeInSeconds {
+                print("Cooking: \(timeInSeconds - self.elapsedTime) seconds remaining")
+            } else {
+                self.elapsedTime = 0.0
+                print("Egg is ready!")
+                timer.invalidate()
+            }
         }
     }
 }
